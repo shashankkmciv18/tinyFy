2f3a50f1477ce106d8d3432d373ac4838c713146
shashankkmciv18 <shashankkmciv18@itbhu.ac.in>
2021-10-15 00:20:44 +0530

    Near to final

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bp.urlencoded({extended:true}));
 // redis
 
 // const fetch=require('node-fetch');
-const redis=require('redis');
+// const redis=require('redis');
 const PORT=process.env.PORT||3000;
 // const REDIS_PORT = process.env.REDIS_PORT || 6379;
 // const client=redis.createClient(REDIS_PORT);
